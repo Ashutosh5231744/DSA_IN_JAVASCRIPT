e50dae84ebf6de93a3692bfac9deb056336eed0b
Ashutosh5231744 <ashutosh91619@gmail.com>
2026-02-06 11:22:29 +0530

    count even number in an array

--- Basic.js
+++ Basic.js
@@ -10,3 +10,14 @@ function largestelement(arr){
    return largest;
 }
 console.log(largestelement([1,2,3,4,5,6,7]));
+
+function counteven(arr){
+  let count=0;
+  for(let i=0;i<arr.length;i++){
+    if(arr[i]%2===0){
+      count++;
+    }
+  }
+  return count;
+}
+console.log(counteven([1,2,3,4]));
