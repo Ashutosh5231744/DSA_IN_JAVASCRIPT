bf960c28cdc61b3932542a3d70f9fde48bb60aa0
Ashutosh5231744 <ashutosh91619@gmail.com>
2026-02-06 11:15:16 +0530

    day 2 of Basic

--- Basic.js
+++ Basic.js
@@ -1,2 +1,12 @@
 console.log("hello world");
 
+function largestelement(arr){
+   let largest=-Infinity;
+   for(let i=0;i<arr.length;i++){
+     if(arr[i]>largest){
+      largest=arr[i];
+     }
+   }
+   return largest;
+}
+console.log(largestelement([1,2,3,4,5,6,7]));
