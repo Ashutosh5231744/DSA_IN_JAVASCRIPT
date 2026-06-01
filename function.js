048bee8e99959ef835a995255f1a5f411abbb03b
Ashutosh5231744 <ashutosh91619@gmail.com>
2026-06-01 11:16:48 +0530

    Revising DSA basic concept

---
+++ function.js
@@ -0,0 +1,32 @@
+// console.log("Hello")
+// // using function 
+// function printing(){
+//   console.log("hello using fuction")
+// }
+// printing();
+
+// function printDynamic(name){
+//   console.log("Hello "+name+" bhai")
+// }
+// printDynamic("Ashutosh")
+
+// // Adding to number 
+
+// function Add(a,b){
+//   let sum=a+b;
+//   console.log(sum)
+// }
+// Add(2,4);
+
+// function square(a){
+//   let value=a*a;
+//   console.log(value)
+// }
+// square(5);
+
+function cube(x){
+  let value=x*x*x;
+  return value;
+}
+ let y=cube(8);
+ console.log(y);
---
+++ ifelse.js
@@ -0,0 +1,17 @@
+function voting(age){
+  if(age>18){
+    console.log("eligible for voting")
+  }else{
+    console.log("not eligible for voting")
+  }
+}
+voting(5);
+
+function checKEVENODD(NUMBER){
+  if(NUMBER%2==0){
+    console.log("even")
+  }else{
+    console.log("odd")
+  }
+}
+checKEVENODD(5)
